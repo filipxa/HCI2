fdc1d99a36dd9c4da5536f555bb4d1d1f27d915d
vlado12310 <vlado12310@gmail.com>
2018-06-02 22:59:08 +0200

    Osnovni entiteti
    nema greske pri pokretanju

---
+++ RacunarskiCentar/RacunarskiCentar/Smer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RacunarskiCentar
+{
+    class Smer : GUIObject
+    {
+        string id;
+        string ime;
+        DateTime datumUvodjenja;
+        string opis;
+
+
+        public string ID
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnValueChanged(new EventArgs());
+            }
+        }
+        public string Ime
+        {
+            get => ime;
+            set
+            {
+                ime = value;
+                OnValueChanged(new EventArgs());
+            }
+        }
+        public string Opis
+        {
+            get => opis;
+            set
+            {
+                opis = value;
+                OnValueChanged(new EventArgs());
+            }
+        }
+
+        public DateTime DatumUvodjenja
+        {
+            get => datumUvodjenja;
+            set
+            {
+                datumUvodjenja = value;
+                OnValueChanged(new EventArgs());
+            }
+        }
+
+        public override GUIObject Copy()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void restoreFromCopy(GUIObject guiObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
